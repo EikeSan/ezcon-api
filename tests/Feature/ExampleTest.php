4f71c1a1c38bfbe19601e2e889c95660c44420c2
Eike Santiago <eikesantz@hotmail.com>
2018-11-22 15:56:43 -0300

    fix(test): edit the basic test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,8 +14,8 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        $response = $this->get('/users/me');
 
-        $response->assertStatus(200);
+        $response->assertStatus(401);
     }
 }
